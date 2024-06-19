3f8485ad57b1ca25c1d49dc1adb53183ec3cdc04
kishantalekar <kishantalekar024@gmail.com>
2024-06-19 09:51:14 +0530

    doen

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+} from "@clerk/clerk-react";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const clerkFrontendApi = "your-clerk-frontend-api-key"; // Replace with your actual Clerk frontend API key
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ClerkProvider
+      frontendApi={"sk_test_SMhZl7HS1tT1hiuDBdAON7sRs5bQAVQyz80EYxxrlM"}
+      publishableKey={"pk_test_Z3Jvd2luZy10dW5hLTM0LmNsZXJrLmFjY291bnRzLmRldiQ"}
+    >
+      <SignedIn>
+        <App />
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </ClerkProvider>
+  </React.StrictMode>
+);
